da8ccb155ed4a1c317c31dca3df9f0eabf4f03ab
Seyed Hossein Hosseini <Seyed.94294@gmail.com>
2026-08-01 15:21:28 +0330

    Add project files.

---
+++ ISCM.Domain/Entities/Asset.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISCM.Domain.Entities;
+
+public class Asset
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Type { get; set; } = string.Empty;
+
+    public string OperatingSystem { get; set; } = string.Empty;
+
+    public string IPAddress { get; set; } = string.Empty;
+
+    public string Location { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; }
+}
