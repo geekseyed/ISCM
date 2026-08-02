f0c10ebc36b0175d6cc2c2dd5c024f4baa95183a
Seyed Hossein Hosseini <Seyed.94294@gmail.com>
2026-08-02 08:23:13 +0330

    feat(domain): add base entity abstraction

---
+++ ISCM.Domain/Common/BaseEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISCM.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; protected set; }
+
+    public DateTime CreatedAt { get; protected set; }
+
+    public DateTime? ModifiedAt { get; protected set; }
+
+    protected BaseEntity()
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public void UpdateModifiedDate()
+    {
+        ModifiedAt = DateTime.UtcNow;
+    }
+}
