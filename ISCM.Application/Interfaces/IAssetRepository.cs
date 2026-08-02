e02ddfb61420ef9ffe2d8d8f5fe25d75ab0bf362
Seyed Hossein Hosseini <Seyed.94294@gmail.com>
2026-08-02 08:48:42 +0330

    (ISCM.Application.csproj) feat(application): create application layer project
    ----------------------------------------------------
    (IAssetRepository.cs)
    feat(application): add asset repository abstraction

---
+++ ISCM.Application/Interfaces/IAssetRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ISCM.Domain.Entities;
+
+namespace ISCM.Application.Interfaces;
+
+public interface IAssetRepository
+{
+    Task<IEnumerable<Asset>> GetAllAsync();
+
+    Task<Asset?> GetByIdAsync(Guid id);
+
+    Task AddAsync(Asset asset);
+}
