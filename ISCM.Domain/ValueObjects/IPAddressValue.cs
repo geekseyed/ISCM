42c30297e64ee7ac016f35700f354dc2046ccf28
Seyed Hossein Hosseini <Seyed.94294@gmail.com>
2026-08-02 08:38:23 +0330

    feat(domain): add asset value objects for ip and operating system

---
+++ ISCM.Domain/ValueObjects/IPAddressValue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ISCM.Domain.ValueObjects;
+
+public class IPAddressValue
+{
+    public string Value { get; private set; }
+
+    private IPAddressValue()
+    {
+        Value = string.Empty;
+    }
+
+
+    public IPAddressValue(string value)
+    {
+        if (!IPAddress.TryParse(value, out _))
+        {
+            throw new ArgumentException("Invalid IP Address");
+        }
+
+        Value = value;
+    }
+
+
+    public override string ToString()
+    {
+        return Value;
+    }
+}
---
+++ ISCM.Domain/ValueObjects/OperatingSystemInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISCM.Domain.ValueObjects;
+
+public class OperatingSystemInfo
+{
+    public string Name { get; private set; }
+
+    public string Version { get; private set; }
+
+
+    private OperatingSystemInfo()
+    {
+        Name = string.Empty;
+        Version = string.Empty;
+    }
+
+
+    public OperatingSystemInfo(
+        string name,
+        string version)
+    {
+        Name = name;
+        Version = version;
+    }
+
+
+    public override string ToString()
+    {
+        return $"{Name} {Version}";
+    }
+}
