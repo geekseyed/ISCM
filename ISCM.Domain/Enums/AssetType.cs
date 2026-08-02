5f45c142f1d0d208122eafae8848bdd1f91e4dc8
Seyed Hossein Hosseini <Seyed.94294@gmail.com>
2026-08-02 08:17:14 +0330

    feat(domain): add AssetType enumeration

---
+++ ISCM.Domain/Enums/AssetType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISCM.Domain.Enums;
+
+public enum AssetType
+{
+    Unknown = 0,
+
+    Workstation = 1,
+
+    Server = 2,
+
+    VirtualMachine = 3,
+
+    RTU = 4,
+
+    Switch = 5,
+
+    Router = 6,
+
+    Firewall = 7,
+
+    SCADAServer = 8,
+
+    DatabaseServer = 9
+}
