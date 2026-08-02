79c653ee45ef82b4016ac11c5d4d846e1ebdd6c6
Seyed Hossein Hosseini <Seyed.94294@gmail.com>
2026-08-02 09:03:21 +0330

    feat(application): add asset service contract With Codes

---
+++ ISCM.Application/Features/Assets/Services/IAssetService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ISCM.Application.Features.Assets.DTOs;
+
+namespace ISCM.Application.Features.Assets.Services;
+
+public interface IAssetService
+{
+    Task<IEnumerable<AssetDto>> GetAllAsync();
+}
