0f75ab16e79397658d8bb9b7046dac728414e368
Seyed Hossein Hosseini <Seyed.94294@gmail.com>
2026-08-02 09:02:55 +0330

    feat(application): add GetAssetsQuery With Codes

---
+++ ISCM.Application/Features/Assets/Queries/GetAssetsQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISCM.Application.Features.Assets.Queries;
+
+public class GetAssetsQuery
+{
+}
