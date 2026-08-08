d38fd14d054e0b7a3d367c54cb50d4e5ac42832c
Seyed Hossein Hosseini <Seyed.94294@gmail.com>
2026-08-08 14:28:42 +0330

    feat(application): add report generation contract

---
+++ ISCM.Application/Interfaces/IReportService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ISCM.Domain.Entities;
+
+namespace ISCM.Application.Interfaces;
+
+public interface IReportService
+{
+    // این متد مسیر فایل ذخیره شده را برمی‌گرداند
+    Task<string> GenerateAndSaveReportAsync(ScanResult scanResult, string outputDirectory);
+}
