9c9d426889296b9b093fd066260cbde22e08bfb6
Seyed Hossein Hosseini <Seyed.94294@gmail.com>
2026-08-08 13:22:18 +0330

    feat(web): register real scanner services in DI container

--- ISCM.Web/Program.cs
+++ ISCM.Web/Program.cs
@@ -1,3 +1,7 @@
+﻿using ISCM.Application.Interfaces;
+using ISCM.Infrastructure.Scanning;
+using ISCM.Infrastructure.Scanning.Checks;
+using ISCM.Infrastructure.Scanning.Collectors;
 using ISCM.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,18 +10,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// ۱. ثبت Collector اطلاعات سیستم
+builder.Services.AddSingleton<WindowsSystemInfoCollector>();
+
+// ۲. ثبت چک‌های امنیتی (هر چکی که ساختیم را اینجا اضافه می‌کنیم)
+builder.Services.AddTransient<IHardeningCheck, FirewallDomainProfileCheck>();
+
+// ۳. ثبت اسکنر اصلی
+builder.Services.AddScoped<IScanService, WindowsHardeningScanner>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
