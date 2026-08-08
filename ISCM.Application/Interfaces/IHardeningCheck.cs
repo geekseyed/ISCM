433ddfca56954cca5167bbc46c4d8ca37d47dc5f
Seyed Hossein Hosseini <Seyed.94294@gmail.com>
2026-08-08 13:05:54 +0330

    feat(application): add scanner and hardening check contracts

---
+++ ISCM.Application/Interfaces/IHardeningCheck.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ISCM.Domain.Entities;
+
+namespace ISCM.Application.Interfaces;
+
+public interface IHardeningCheck
+{
+    string CheckId { get; }
+    Task<Finding> EvaluateAsync();
+}
